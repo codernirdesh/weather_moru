76e7be39fd10acbccba01f31bde94da04cf34d2d
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2022-11-03 17:52:22 +0545

    Added persistance on skip button.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_moru/providers/app_info_provider.dart';
 import 'package:weather_moru/providers/weather_data_provider.dart';
 import 'package:weather_moru/routes/routes_name.dart';
 import 'routes/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool? hasPressedSkip = prefs.getBool("hasPressedSkip");
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => AppInfoProvider()),
       ChangeNotifierProvider(create: (_) => WeatherDataProvider()),
     ],
-    child: const WeatherApp(),
+    child: WeatherApp(
+      hasPressedSkip: hasPressedSkip ?? false,
+    ),
   ));
 }
 
 class WeatherApp extends StatelessWidget {
-  const WeatherApp({Key? key}) : super(key: key);
+  final bool hasPressedSkip;
+  const WeatherApp({Key? key, required this.hasPressedSkip}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       onGenerateRoute: Routes.generateRoute,
-      initialRoute: RoutesName.help,
+      // if hasPressedSkip is true, then we go to the home screen
+      initialRoute: hasPressedSkip ? RoutesName.home : RoutesName.help,
     );
   }
 }
