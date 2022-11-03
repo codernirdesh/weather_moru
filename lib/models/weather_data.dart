82c49f71fa4a3535ae84d7ebc420af6d1aef210b
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2022-11-03 17:33:54 +0545

    Removed Unused dependency

--- lib/models/weather_data.dart
+++ lib/models/weather_data.dart
@@ -1,8 +1,6 @@
 // To parse this JSON data, do
 //
 //     final weatherData = weatherDataFromMap(jsonString);
-
-import 'package:meta/meta.dart';
 import 'dart:convert';
 
 WeatherData weatherDataFromMap(String str) =>
